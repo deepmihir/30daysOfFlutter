7d459f8b53dbd61ab7fd17cf0140b1d9b6784e8c
deepmihir <deepmihir@gmail.com>
2022-05-15 13:25:36 +0530

    Day 12 - ListView Builder List Generate Card & Asserts 30DaysOfFlutter

---
+++ lib/Widgets/item_widgets.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_catalogue/models/catalogue.dart';
+
+class ItemWidget extends StatelessWidget {
+  final Item item;
+  const ItemWidget({Key? key, required this.item}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+          onTap: () {
+            print("Iphone 12 pressed");
+          },
+          leading: Image.network(item.image),
+          title: Text("Iphone 12 pro"),
+          subtitle: Text(item.desc),
+          trailing: Text(
+            "\$${item.price}",
+            textScaleFactor: 1.2,
+            style: TextStyle(
+              color: Colors.deepPurple,
+              fontWeight: FontWeight.bold,
+            ),
+          )),
+    );
+  }
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalogue/Widgets/drawer.dart';
+import 'package:flutter_catalogue/Widgets/item_widgets.dart';
+import 'package:flutter_catalogue/models/catalogue.dart';
 
 class HomePage extends StatelessWidget {
   final int days = 30;
@@ -7,13 +9,20 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final dummylist = List.generate(20, (index) => CatalogueModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text("Catalogue app"),
       ),
-      body: Center(
-        child: Container(
-          child: Text("Welcome to $days of flutter $name!"),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummylist.length,
+          itemBuilder: (BuildContext context, int index) {
+            return ItemWidget(
+              item: dummylist[index],
+            );
+          },
         ),
       ),
       drawer: Mydrawer(),
